a94f451f84b291bf5172293d0e35188939f07128
wjwarner24 <wjwarner24@gmail.com>
2023-12-30 13:28:00 -0800

    changed many things in the code, user uploads are now written to a database where sql commands are executed on instead of in memory. Project now fully works as intended.

--- __init__.py
+++ __init__.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template, request, send_file, session
+from flask import Flask, render_template, request, session, Response
 import pandas as pd
 import sqlite3
 from openai import OpenAI
-from io import BytesIO
+from io import StringIO
+import uuid
+from sqlalchemy import create_engine
+import csv
+import os
 
 app = Flask(__name__)
 client = OpenAI()
-
+print(os.getcwd())
+database_path = 'sqlite:////var/www/webApp/webApp/data.db'
+engine = create_engine(database_path)
 @app.route('/about')
 def about():
     return render_template('about.html')
@@ -24,31 +30,38 @@ def upload():
     if file.filename == '':
         return 'No selected file'
     if file:
+        table_id = str(uuid.uuid4())
+        table_id = f'a{table_id}'
+        table_id = table_id.replace('-', '')
+
+        session.pop('id', None)
+        session.pop('name', None)
+        session['id'] = table_id
+        session['name'] = file.filename
+
         user_data = pd.read_csv(file)
-        set_df(user_data)
-        
-        return render_template('index.html', data_exists=True, result_exists=False, num_rows = user_data.shape[0], data=user_data.head(5))
+        user_data.to_sql(table_id, con=engine, index=False)
+        num_rows, columns, first_five_rows = get_display_data()
+
+        return render_template('index.html', data_exists=True, result_exists=False, num_rows=num_rows, data=first_five_rows, display_columns=columns)
 
 
 #Function when user presses 'execute query' button
 @app.route('/query', methods=['POST'])
 def run_query():
-    user_data = get_df()
+    
     user_query = request.form['user_query']
     
-    # Build context
-    columns = user_data.columns.tolist()
-    sample_data = user_data.iloc[:5, :]
-    context = f"The table has columns: {', '.join(columns)}. "
-    context += f"For example, the first five rows are: {sample_data}. "
-
+    row_count, column_names, first_five_rows = get_display_data()
+    context = f"The table has {row_count} rows, and it has columns: {column_names}. For example, the first five rows are: {first_five_rows}"
+    
     # Call OpenAI API
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
             { "role": "system", "content": f'''Your task is to translate a query written in plain english by a user into a valid, properly formatted sql query on an sql table hosted on sqlite3.
             The accuracy of your query is of the upmost importance. {context} Your output should contain only the sql query. If your entire output was ran against an sql table it should
-            be valid. There should be no other text or comments in your sql query output. Do not include newline symbols. the table is called 'csv_data'. The names of the columns in this 
+            be valid. There should be no other text or comments in your sql query output. Do not include newline symbols. the table is called '{session.get('id')}'. The names of the columns in this 
             sql table are exactly what i specified earlier. Keep any spaces or any other characters that were in the column names when you are writing the sql query. The column names 
             specified earlier are the exact column names in the sql table. Make sure to include ';' at the end of the sql query. Your response should only include one sql query, 
             respondong with multiple queries will cause the program to fail.'''},
@@ -59,77 +72,103 @@ def run_query():
     
     sql_query = response.choices[0].message.content
     update_query = False
+    query_column_names=""
    
-    # Try to create a sqlite database from the dataframe and execute_user_query
     try:
-        connection = sqlite3.connect(":memory:")
-        user_data.to_sql("csv_data", connection, index=False, if_exists='replace') #creates sql table in memory
-        result, column_names = execute_user_query(sql_query, connection)
-        
-        #if the query was one that modified the table, update the dataframe
+        result, query_column_names = execute_user_query(sql_query)
         if not sql_query.lower().startswith("select"):
             column_names = []
             result = []
-            temp_query = "SELECT * FROM csv_data;"
-            updated_data = pd.read_sql_query(temp_query, connection)
-            set_df(updated_data)
-            user_data = updated_data
             update_query = True
             
-        connection.close()
+        
     except Exception as e:
-        connection.close()
         print(e)
         print(sql_query)
         result = []
         column_names = []
-        sql_query += " \nERROR WHILE EXECUTING THIS. PLEASE TRY AGAIN."
+        sql_query = "ERROR WHILE EXECUTING THIS. PLEASE TRY AGAIN. " + sql_query
     
-    return render_template('index.html', data_exists=True, result_exists=True, num_rows=user_data.shape[0], data=user_data.head(5), result=result, column_names=column_names, query=sql_query, update_query=update_query)
+    num_rows, column_names, first_five_rows = get_display_data()
+
+    return render_template('index.html', data_exists=True, result_exists=True, num_rows=num_rows, data=first_five_rows, result=result, column_names=query_column_names, display_columns=column_names, query=sql_query, update_query=update_query)
 
 
 # Executes query on the sql table, return the result and the column names
-def execute_user_query(sql_query, connection):
-    cursor = connection.cursor()
+def execute_user_query(sql_query):
+    conn = sqlite3.connect('/var/www/webApp/webApp/data.db')
+    cursor = conn.cursor()
     cursor.execute(sql_query)
     result = cursor.fetchall()
-    connection.commit()
+    conn.commit()
     try:
         columns = [description[0] for description in cursor.description]
     except Exception as e:
         columns = []
-        
+    conn.close() 
     return result, columns
 
 
-# Function to download csv file
 @app.route('/download_file', methods=['GET'])
 def download_file():
-    csv_buffer = BytesIO()
+    # Connect to the SQLite database
+    conn = sqlite3.connect('/var/www/webApp/webApp/data.db')
+    cursor = conn.cursor()
+    table_id = session.get('id')
+    query = f'SELECT * FROM {table_id}'
+    # Execute a SELECT query to get all data from the table
+    cursor.execute(query)
+    rows = cursor.fetchall()
 
-    # Write the DataFrame to the buffer as a CSV file
-    user_data = get_df()
-    user_data.to_csv(csv_buffer, index=False)
+    # Prepare CSV data
+    csv_data = StringIO()
+    csv_writer = csv.writer(csv_data)
+    
+    # Write header
+    header = [description[0] for description in cursor.description]
+    csv_writer.writerow(header)
 
-    # Set the buffer position to the beginning
-    csv_buffer.seek(0)
+    # Write data
+    csv_writer.writerows(rows)
 
-    # Create a Flask response with the CSV file
-    return send_file(
-        csv_buffer,
-        mimetype='text/csv',
-        as_attachment=True,
-        download_name="download_file.csv"
+    # Close the database connection
+    conn.close()
+    updated_name = session.get('name') + '_updated'
+    # Prepare the response
+    response = Response(
+        csv_data.getvalue(),
+        content_type='text/csv',
+        headers={'Content-Disposition': f'attachment; filename={updated_name}.csv'}
     )
 
-def get_df():
-    user_data_dict = session.get('user_data', {})
-    user_data = pd.DataFrame(user_data_dict)
-    return user_data
+    return response
 
-def set_df(df):
-    user_data_dict = df.to_dict()
-    session['user_data'] = user_data_dict
+# Get the data necessary to display a preview of the table: number of rows, column names, and the first five rows
+def get_display_data():
+
+    table_id = session.get('id')
+    row_count_query = f"SELECT COUNT(*) FROM {table_id};"
+    column_names_query = f'PRAGMA table_info({table_id});'
+    first_five_rows_query = f'SELECT * FROM {table_id} LIMIT 5;'
+    conn = sqlite3.connect('/var/www/webApp/webApp/data.db')
+    cursor = conn.cursor()
+
+    cursor.execute(row_count_query)
+    row_count = cursor.fetchall()
+    row_count = row_count[0][0]
+
+    cursor.execute(column_names_query)
+    column_names = cursor.fetchall()
+
+    cursor.execute(first_five_rows_query)
+    first_five_rows = cursor.fetchall()
+    conn.close()
+
+    columns = []
+    for list in column_names:
+        columns.append(list[1])
+
+    return row_count, columns, first_five_rows
 
 
 if __name__ == '__main__':
